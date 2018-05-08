c7aaedd7c28a54aea3ff63fbaf2cd882c5e99653
TMKangwantas <tmkangwantas@gmail.com>
2018-05-08 01:44:37 -0400

    Add files via upload

---
+++ src/TodoList.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: []
+    };
+
+    this.addItem = this.addItem.bind(this);
+  }
+
+  addItem(e) {
+  }
+  render() {
+    return (
+      <div className="todoListMain">
+        <div className="header">
+          <form>
+            <input ref={(a) => this._inputElement = a} 
+            </input>
+            <button type="submit">Add</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
