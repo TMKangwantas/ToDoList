b307ee7aea61b03323ed8e5a37c45670b292be8d
TMKangwantas <tmkangwantas@gmail.com>
2018-05-08 03:01:28 -0400

    Add files via upload

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,20 +2,27 @@ import React, { Component } from "react";
 import TodoItems from "./TodoItems";
 import "./TodoList.css";
 
-class TodoList extends Component {
-  constructor(props) {
+class TodoList extends Component
+{
+  constructor(props)
+  {
     super(props);
 
-    this.state = {
+    this.state =
+    {
       items: []
     };
 
     this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
   }
 
-  addItem(e){
-      if(this._inputElement.value !== ""){
-          var newItem = {
+  addItem(e)
+  {
+      if(this._inputElement.value !== "")
+      {
+          var newItem =
+          {
               text: this._inputElement.value,
               key: Date.now()
           };
@@ -33,20 +40,35 @@ class TodoList extends Component {
       e.preventDefault();
   }
 
-  render() {
-  return (
-    <div className="todoListMain">
-      <div className="header">
-        <form onSubmit={this.addItem}>
-          <input ref={(a) => this._inputElement = a}>
-          </input>
-          <button type="submit">Add</button>
-        </form>
+  deleteItem(key)
+  {
+    var filteredItems = this.state.items.filter(function (item)
+    {
+      return (item.key !== key);
+    });
+
+    this.setState(
+      {
+      items: filteredItems
+      });
+    }
+
+  render()
+  {
+    return(
+      <div className="todoListMain">
+        <div className="header">
+          <form onSubmit={this.addItem}>
+            <input ref={(a) => this._inputElement = a}>
+            </input>
+            <button type="submit">Add</button>
+          </form>
+        </div>
+        <TodoItems entries={this.state.items}
+            delete={this.deleteItem}/>
       </div>
-      <TodoItems entries={this.state.items}/>
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default TodoList;
